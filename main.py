c8f3373f6bd2fc3a93a5bafd68fb7c2e98236bfc
Xiaotong Xu <xxu35@slb.com>
2026-04-14 14:24:16 +0800

    format response with pytantic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
 
@@ -7,22 +9,17 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 
-# TavilySearch is a tool that searches over internet built on top of the Tavily API witch is integrated with LangChain
 from langchain_tavily import TavilySearch
 
-# from tavily import TavilyClient 
-# tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
-# @tool
-# def search(query: str) -> str:
-#     """
-#     Tool that searches over internet
-#     Args:
-#         query: The query to search for
-#     Returns:
-#         The search results
-#     """
-#     print(f"Searching for: {query}")
-#     return tavily.search(query=query)
+# The point of this class here is to represent the source of the answer
+class Source(BaseModel): 
+    """Schema for a source used by the agent"""
+    url: str = Field(description="The url of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the response of the agent"""
+    answer: str = Field(description="The answer to the question")
+    sources: List[Source] = Field(description="The list of sources to generate the answer")
 
 
 llm = ChatOpenAI(
@@ -31,15 +28,18 @@ llm = ChatOpenAI(
     openai_api_key=os.getenv("ZAI_API_KEY"),
     openai_api_base="https://open.bigmodel.cn/api/paas/v4/",
 )
-# tools = [search]
-tools = [TavilySearch()] # 在 LangSmith 上看 trace 信息可以看到，被调用的 tool name is tavily_search, this is their naming convention
-agent = create_agent(model=llm, tools=tools)
+
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke(
-        {"messages": HumanMessage(content="What is the weather in Tokyo?")}
+        {"messages": HumanMessage(
+                content="search for the top 3 job postings as an ai engineer in the last 30 days"
+            )
+        }
     )
     print(result)
 
